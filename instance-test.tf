61230d947c2c774c140c40e4f3d2b2851e5a835d
ohidalgo <101749097+rduserjob@users.noreply.github.com>
2024-09-30 16:03:00 -0300

    Update instance-test.tf

--- instance-test.tf
+++ instance-test.tf
@@ -7,6 +7,7 @@ provider "aws" {
 variable "ami_id" {
   description = "ami-0d53d72369335a9d6"
   type        = string
+  default     = "ami-0d53d72369335a9d6"
 }
 
 variable "instance_type" {
@@ -18,16 +19,19 @@ variable "instance_type" {
 variable "key_name" {
   description = "Demo-duty"
   type        = string
+  default     = "Demo-duty"
 }
 
 variable "subnet_id" {
   description = "subnet-f8fbfca3"
   type        = string
+  default     = "subnet-f8fbfca3"
 }
 
 variable "vpc_id" {
   description = "vpc-88a199ef"
   type        = string
+  default     = "vpc-88a199ef"
 }
 
 variable "disk_size" {
